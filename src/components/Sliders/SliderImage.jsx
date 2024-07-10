694ee1662348bd8785a2bb93c7668328ab27f9aa
huynh <nguyenhuynhdt37@gmail.com>
2024-07-10 23:27:11 +0700

    púh

--- src/components/Sliders/SliderImage.jsx
+++ src/components/Sliders/SliderImage.jsx
@@ -1,8 +1,9 @@
-const SliderImage = ({ image, ...prev }) => {
-  const [currentSlide, setCurrentSlide] = useState(images[0].link);
-
-  console.log(prev);
-  return <>{/* <img src={image.url} className="w-100" alt="" /> */}</>;
+const SliderImage = ({ image }) => {
+  return (
+    <>
+      <img  src={image.url} className="w-100" alt="" />{" "}
+    </>
+  );
 };
 
 export default SliderImage;
